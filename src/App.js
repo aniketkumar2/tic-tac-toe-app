80af4c69d0348bef191c1edde9ca0ed644e506b6
Aniket <aniketkumarmdh62@gmail.com>
2025-08-19 15:48:48 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import TicTacToe from "./TicTacToe";
 import GameRulesDialog from "./GameRulesDialog";
 import { Button } from "@mui/material";
 import "./App.css";
-
+//
 function App() {
   const [openRules, setOpenRules] = useState(false);
 
